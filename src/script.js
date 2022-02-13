e1d3bd52013c3c2f6ccad9773435155bd05e58af
OrtizBrayan <brayanortizbastidas@gmail.com>
2022-02-12 23:10:19 -0500

    complete base code

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 let routes = {};
 let templates = {};
-let app_div = document.getElementById('app');
+let app_div = document.getElementById("app");
 
-function home(){
+function home() {
     let div = document.createElement('div');
     let link = document.createElement('a');
     link.href = '#/about';
@@ -12,59 +12,60 @@ function home(){
     div.appendChild(link);
 
     app_div.appendChild(div);
-}
+};
 
-function about(){
-    let div = document.createElement('div')
-    let link = document.createElement('a')
-    link.href = '#/'
-    link.innerText = 'Home'
+function about() {
+    let div = document.createElement('div');
+    let link = document.createElement('a');
+    link.href = '#/';
+    link.innerText = 'Home';
 
-    div.innerHTML = '<h1>About</h1>'
-    div.appendChild(link)
+    div.innerHTML = '<h1>About</h1>';
+    div.appendChild(link);
 
-    app_div.appendChild(div)
-}
+    app_div.appendChild(div);
+};
 
-function route(path, template){
-    if(typeof template === 'function'){
-        return routes[path] = template
-    } else if(typeof template === 'string'){
-        return routes[path] = templates[template]
-    } else {
-        return
+function route (path, template) {
+    if (typeof template === 'function') {
+        return routes[path] = template;
     }
-}
+    else if (typeof template === 'string') {
+        return routes[path] = templates[template];
+    } else {
+        return;
+    };
+};
 
-function template (name, templateFuntion){
-    return template[name] = templateFuntion
-}
+function template (name, templateFunction) {
+    return templates[name] = templateFunction;
+};
 
 template('home', function(){
     home();
-})
+});
 
 template('about', function(){
     about();
-})
+});
 
-route('/','home')
-route('/about', 'about')
+route('/', 'home');
+route('/about', 'about');
 
-function resolveRoute(route){
-    try{
-        return routes[route]
-    } catch(e){
-        throw new Error('Route ${Root} not found')
-    }
-}
+function resolveRoute(route) {
+    try {
+        return routes[route];
+    } catch (e) {
+        throw new Error(`Route ${route} not found`);
+    };
+};
 
-function router(evt){
-    let url = window.location.hash.slice(1) || '/'
-    let route = resolveRoute(url)
+function router(evt) {
+    let url = window.location.hash.slice(1) || '/';
+    let route = resolveRoute(url);
 
-    route()
-}
+    route();
+};
 
-window.addEventListener('load', router)
-window.addEventListener('hashchange', router)
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);

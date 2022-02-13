07ac72966ffac1dbd6321cb73614a933e9db9330
OrtizBrayan <brayanortizbastidas@gmail.com>
2022-02-12 20:40:10 -0500

    First construction of code in JS file

---
+++ Script.js
@@ -0,0 +1,68 @@
+let routes = {};
+let templates = {};
+let app_div = document.getElementById('app');
+
+function home(){
+    let div = document.createElement('div');
+    let link = document.createElement('a');
+    link.href = '#/about';
+    link.innerText = 'about';
+
+    div.innerHTML = '<h1>Home</h1>';
+    div.appendChild(link);
+
+    app_div.appendChild(div);
+}
+
+function about(){
+    let div = document.createElement('div')
+    let link = document.createElement('a')
+    link.href = '#/'
+    link.innerText = 'home'
+
+    div.innerHTML = '<h1>About</h1>'
+    div.appendChild(link)
+
+    app_div.appendChild(div)
+}
+
+function route(path, template){
+    if(typeof template === 'function'){
+        return routes[path] = template
+    } else if(typeof template === 'string'){
+        return routes[path] = templates[template]
+    } else {
+        return
+    }
+}
+
+function template (name, templateFuntion){
+    return template[name] = templateFuntion
+}
+
+template('home', function(){
+    home();
+})
+
+template('about', function(){
+    about();
+})
+
+route('/','home')
+route('/about', 'about')
+
+function resolveRoute(route){
+    try{
+        return routes[route]
+    } catch(e){
+        throw new Error('Route ${Root} not found')
+    }
+}
+
+function route(evt){
+    let url = window.location.hash.slice(1) || '/'
+    let route = resolveRoute(url)
+
+    route()
+}
+
